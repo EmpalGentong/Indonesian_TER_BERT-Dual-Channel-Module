28ebf361b3df054e7342513ac61ed3b582b3beb6
Empalgentong <saihan507@gmail.com>
2023-03-10 17:46:26 +0700

    add Load BERTmodel & BERTtokenizer

--- code/main.py
+++ code/main.py
@@ -1,3 +1,9 @@
 import torch
-print(torch.__version__)
+from transformers import BertTokenizer, AutoModel
+
+tokenizer = BertTokenizer.from_pretrained("indobenchmark/indobert-base-p2")
+bertModel = AutoModel.from_pretrained("./BERTmodel/")
+
+x = torch.LongTensor(tokenizer.encode('aku adalah anak [MASK]')).view(1,-1)
+print(x, bertModel(x)[0].sum())
 
